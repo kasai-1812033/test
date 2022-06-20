3332cca6e6992186506e77a9ad48e161965ec4ed
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2022-06-20 10:04:43 +0900

    Update multi_coverage.m

--- multi_coverage.m
+++ multi_coverage.m
@@ -2,7 +2,7 @@
 clc; clear; close all;
 N = 8; %number of agents
 dim = 3; %次元
-p = 0.2*rand(dim*N,1); %初期位置
+p = 0.1*rand(dim*N,1); %初期位置
 P = p'; %初期位置の転置（1行1ステップ）
 alpha = 1; %正定値
 %% voronoi
@@ -51,39 +51,42 @@ end
 if dim==2
     J = @(p) quad2d(@(X,Y) phiv(X,Y,p),0,1,0,1,'Singular',false); %2次元均一配置分布
 else
-    J = @(p) integral3(@(X,Y,Z) phiv(X,Y,Z,p),0,1,0,1,0,1); %3次元均一配置分布
+    J = @(p) integral3(@(X,Y,Z) phiv(X,Y,Z,p),0,0.5,0,0.5,0,0.5); %3次元均一配置分布
 end
 
 %入力式のdJdpの計算
-%numjacはや媚案を数値計算するMATLAB内の標準関数
+%numjacはヤコビアンを数値計算するMATLAB内の標準関数
 dJdp = @(p) numjac( @(t,p) J(p),0,p,J(p),1e-6*ones(dim*N,1),[],0);
 
-%% 2次元用
-% for k=1:100
-%     clf
-%     fcontour(@(X,Y) phiv(X,Y,p),[0,1,0,1],'r','LevelStep',1e-2); %エージェント周りの等高線
-%     axis equal
-%     hold on
-%     title(sprintf('k=%d',k)); %1ステップごとにタイトルに表示
-%     plot(P(:,1:2:end-1),P(:,2:2:end),'.-'); %エージェントの位置
-%     drawnow %この時点で描画
-% 
-%     p = p - alpha * dJdp(p)'; %状態更新
-%     P(end+1,:) = p; %更新した位置の追記
-% end
-
-%% 3次元用
-for k=1:100
-    clf
-%     fsurf(@(X,Y,Z) phiv(X,Y,p)); %エージェント周りの等高線
-    axis equal
-    grid on
-    hold on
-    view(10,-25);
-    title(sprintf('k=%d',k)); %1ステップごとにタイトルに表示
-    plot3(P(:,1:3:end-2),P(:,2:3:end-1),P(:,3:3:end),'.-'); %エージェントの位置
-    drawnow %この時点で描画
-
-    p = p - alpha * dJdp(p)'; %状態更新
-    P(end+1,:) = p; %更新した位置の追記
+%% plot
+if dim==2
+    % 2次元用
+    for k=1:100
+        clf
+        fcontour(@(X,Y) phiv(X,Y,p),[0,1,0,1],'r','LevelStep',1e-2); %エージェント周りの等高線
+        axis equal
+        hold on
+        title(sprintf('k=%d',k)); %1ステップごとにタイトルに表示
+        plot(P(:,1:2:end-1),P(:,2:2:end),'.-'); %エージェントの位置
+        drawnow %この時点で描画
+    
+        p = p - alpha * dJdp(p)'; %状態更新
+        P(end+1,:) = p; %更新した位置の追記
+    end
+else
+    % 3次元用
+    for k=1:100
+        clf
+    %     fsurf(@(X,Y,Z) phiv(X,Y,p)); %エージェント周りの等高線
+        axis equal
+        grid on
+        hold on
+        view(10,-25);
+        title(sprintf('k=%d',k)); %1ステップごとにタイトルに表示
+        plot3(P(:,1:3:end-2),P(:,2:3:end-1),P(:,3:3:end),'.-'); %エージェントの位置
+        drawnow %この時点で描画
+    
+        p = p - alpha * dJdp(p)'; %状態更新
+        P(end+1,:) = p; %更新した位置の追記
+    end
 end
