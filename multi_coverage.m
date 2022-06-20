74905177e3c62bab5ae150fb6557bcce735b628f
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2022-06-20 16:32:56 +0900

    update multi_coverage
    ・エージェント周りのボロノイ領域の算出
    ・各ボロノイ領域の端点から領域積分
    ・とりあえず動くように

--- multi_coverage.m
+++ multi_coverage.m
@@ -2,37 +2,9 @@
 clc; clear; close all;
 N = 8; %number of agents
 dim = 3; %次元
-p = 0.1*rand(dim*N,1); %初期位置
+p = 0.2*rand(dim*N,1); %初期位置
 P = p'; %初期位置の転置（1行1ステップ）
 alpha = 1; %正定値
-%% voronoi
-% clc; clear; close all;
-% x=[0.2,0.6,0.9,0.7,0.8,0.5,0.4,0.1,0.3,0.5];
-% y=[0.7,0.9,0.8,0.6,0.3,0.4,0.2,0.1,0.3,0.5];
-% x = P(:,1:2:end);
-% y = P(:,2:2:end);
-% X = [x',y'];
-% [VX,VY] = voronoi(x,y);
-% h = plot(VX,VY,'-b',X(:,1),X(:,2),'ro');
-% 
-% % Assign labels to the points.
-% nump = size(X,1);
-% plabels1 = arrayfun(@(n) {sprintf('x%d', n)}, (1:nump)');
-% hold on
-% Hp1 = text(X(:,1)-0.05, X(:,2), plabels1, 'Color','r','FontWeight', ...
-%       'bold','HorizontalAlignment','center', ...
-%       'BackgroundColor', 'none');
-% 
-% % Assign labels to the voronoi areas.
-% plabels2 = arrayfun(@(n) {sprintf('C%d', n)}, (1:nump)');
-% Hp2 = text(X(:,1)+0.03, X(:,2)-0.03, plabels2, 'Color','b','FontWeight', ...
-%       'bold', 'HorizontalAlignment','center', ...
-%       'BackgroundColor', 'none');
-% hold off
-% 
-% axis equal
-% xlim([0,1]);
-% ylim([0,1]);
 %% calculation
 %重み関数
 phi  = @(q,p) min(sqrt(sum((q - reshape(p,dim,N)).^2))); %ℓ2ノルム
@@ -56,9 +28,20 @@ end
 
 %入力式のdJdpの計算
 %numjacはヤコビアンを数値計算するMATLAB内の標準関数
-V = @(X,Y,Z) arrayfun(@(x,y,z) phi([x,y,z]',p),X,Y,Z);
-mass = @(p) integral3(@(X,Y,Z) V(X,Y,Z),0,0.5,0,0.5,0,0.5);
-cent = @(p) integral3(@(X,Y,Z) V(X,Y,Z),0,0.5,0,0.5,0,0.5)/mass(p);
+for i=1:N
+    x(i,:)=p(3*i-2); %エージェントのx座標
+    y(i,:)=p(3*i-1); %エージェントのy座標
+    z(i,:)=p(3*i); %エージェントのz座標
+end
+X = [x,y,z;0,0,0;0,1,0;1,0,0;1,1,0;0,0,1;0,1,1;1,0,1;1,1,1]; %ボロノイ分割用の座標ベクトル
+[v,c] = voronoin(X); %3次元ボロノイ分割
+for i=1:N
+    V{i} = alphaShape([v(c{i},1),v(c{i},2),v(c{i},3)]); %エージェント周りのボロノイ空間
+end
+% 質量
+mass = @(p,i) integral3(@(X,Y,Z) phiv(X,Y,Z,p),min(V{i}.Points(:,1)),max(V{i}.Points(:,1)),min(V{i}.Points(:,2)),max(V{i}.Points(:,2)),min(V{i}.Points(:,3)),max(V{i}.Points(:,3)));
+% 重心
+cent = @(p,i) integral3(@(X,Y,Z) phiv(X,Y,Z,p),min(V{i}.Points(:,1)),max(V{i}.Points(:,1)),min(V{i}.Points(:,2)),max(V{i}.Points(:,2)),min(V{i}.Points(:,3)),max(V{i}.Points(:,3)))/mass(p,i);
 % dJdp = @(p) numjac( @(t,p) J(p),0,p,J(p),1e-6*ones(dim*N,1),[],0); %標準関数
 % dJdp = @(p) 2*mass(p)*(p-cent(p));
 
@@ -91,7 +74,7 @@ else
             plot3(P(:,1:3:end-2),P(:,2:3:end-1),P(:,3:3:end),'.-'); %エージェントの位置
             drawnow %この時点で描画
         
-            p = p - alpha * (2*mass(p)*(p-cent(p)))'; %状態更新
+            p = p - alpha * (2*mass(p,i)*(p-cent(p,i))); %状態更新でmassとcentを使って計算
         end
         P(end+1,:) = p; %更新した位置の追記
     end
