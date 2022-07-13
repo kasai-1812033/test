274f66632cd3c8b5d6e5305ca96b6d9b9ad4b779
笠井茉莉 <84959305+kasai-1812033@users.noreply.github.com>
2022-07-13 15:33:16 +0900

    Update multi_coverage.m

--- multi_coverage.m
+++ multi_coverage.m
@@ -8,7 +8,7 @@ P = p'; %初期位置の転置（1行1ステップ）
 alpha = 1; %正定値
 ts = 0;
 dt = 1/30;
-te = 20;
+te = 1;
 %% calculation
 t = ts;
 step = 1;
@@ -19,7 +19,7 @@ while round(t,5)<=te
         py(i,:)=p(3*i-1); %エージェントのy座標
         pz(i,:)=p(3*i); %エージェントのz座標
     end
-    Ps = -[1,1,1]+ 3*[0,0,0;0,1,0;1,0,0;1,1,0;0,0,1;0,1,1;1,0,1;1,1,1]; %ボロノイ分割用の座標ベクトル
+    Ps = -4*[1,1,1]+ 8*[0,0,0;0,1,0;1,0,0;1,1,0;0,0,1;0,1,1;1,0,1;1,1,1]; %ボロノイ分割用の座標ベクトル
     Ps = [px,py,pz;Ps];
     logger.Ps{step} = Ps;
     [v,c] = voronoin(Ps); %3次元ボロノイ分割
@@ -67,63 +67,120 @@ while round(t,5)<=te
         % 重心
         cent = dmass/mass;
         logger.cent{i,step} = cent;
+
+%         if mod(step,(1/dt)*1) == 1
+%             figure(8)
+%             title(['t=',num2str(logger.time(step)),'s'],'FontSize',25);
+%             hold on
+%             
+%             view(45,45);
+%             daspect([1 1 1]);
+%             ax = gca;
+%             ax.Box = 'on';
+%             ax.GridColor = 'k';
+%             ax.GridAlpha = 0.4;
+%             xlabel('x [m]','FontSize',12);
+%             ylabel('y [m]','FontSize',12);
+%             zlabel('z [m]','FontSize',12);
+%             xlim([-2,2]);
+%             ylim([-2,2]);
+%             zlim([-2,2]);
+%             grid on
+%     
+%             plot3(P(step,1:3:end-2),P(step,2:3:end-1),P(step,3:3:end),'k*','MarkerSize',10); %エージェントの位置
+%             
+%             trisurf(k{i},v(c{i},1),v(c{i},2),v(c{i},3),'Facecolor','r','Facealpha',alpha*0.3);
+%     %         plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'g.');
+%                 
+%             
+%             plot3(cent(1),cent(2),cent(3),'bo','MarkerSize',10);
+%             
+%     
+%             hold off
+%         end
             
         p(3*i-2:3*i) = p(3*i-2:3*i) - 0.1 * (p(3*i-2:3*i)-cent'); %状態更新でmassとcentを使って計算
-        P(end+1,:) = p; %更新した位置の追記
     end
+    P(end+1,:) = p; %更新した位置の追記
     t = t + dt
     step = step + 1;
 end
 logger.P = P;
 %% 動画
+close all;
+pt = 15; %FontSize
 figure(1)
+Fig.No = 1;
 time = 1;
-v = VideoWriter(['3D voronoiFormationSample N = ',num2str(N)],'MPEG-4');
+v = VideoWriter(['3D voronoiFormation N = ',num2str(N)],'MPEG-4');
 v.FrameRate = 30;
 open(v);
 while time <= numel(logger.time)
     clf(figure(1))
     disp('.');
-    vert = -2*[1 1 1] + 4*[0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1];
-    [k_all,av_all] = convhull(vert(:,1),vert(:,2),vert(:,3),'Simplify',true);
-    s1 = trisurf(k_all,vert(:,1),vert(:,2),vert(:,3),'FaceColor','k','FaceAlpha',0.1);
-%     fac = [1 2 6 5;2 3 7 6;3 4 8 7;4 1 5 8;1 2 3 4;5 6 7 8];
-%     s = patch('Vertices',vert,'Faces',fac,'FaceVertexCData',hsv(6),'FaceColor','k','FaceAlpha',0.1);
 
     hold on
-    view(-10,25);
+    view(45,45); %normal
+%     view(0,90); %top
+%     view(0,0); %front
     daspect([1 1 1])
     ax = gca;
     ax.Box = 'on';
     ax.GridColor = 'k';
     ax.GridAlpha = 0.4;
-    xlabel('x [m]');
-    ylabel('y [m]');
-    zlabel('z [m]');
-    xlim([-3,3]);
-    ylim([-3,3]);
-    zlim([-3,3]);
+    xlabel('x [m]','FontSize',pt);
+    ylabel('y [m]','FontSize',pt);
+    zlabel('z [m]','FontSize',pt);
+    xlim([-2,2]);
+    ylim([-2,2]);
+    zlim([-2,2]);
     grid on;
 
-    title(sprintf('k=%f',logger.time(time))); %1ステップごとにタイトルに表示
-    plot3(logger.P(time,1:3:end-2),logger.P(time,2:3:end-1),logger.P(time,3:3:end),'k*'); %エージェントの位置
+    title(sprintf('t=%fs',logger.time(time)),'FontSize',25); %1ステップごとにタイトルに表示
+    plot3(logger.P(time,1:3:end-2),logger.P(time,2:3:end-1),logger.P(time,3:3:end),'k*','MarkerSize',10); %エージェントの位置
     
     
     for i = 1:N
-        if i == 1
-%             plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'g.');
-            s2=trisurf(logger.k{i,time},logger.v{time}(logger.c{time}{i},1),logger.v{time}(logger.c{time}{i},2),logger.v{time}(logger.c{time}{i},3),'Facecolor','c','Facealpha',alpha*0.3);
-        elseif i==2
-%             plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'c.');
-            s3=trisurf(logger.k{i,time},logger.v{time}(logger.c{time}{i},1),logger.v{time}(logger.c{time}{i},2),logger.v{time}(logger.c{time}{i},3),'Facecolor','r','Facealpha',alpha*0.3);
-        else
-%             plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'r.');
-            s4=trisurf(logger.k{i,time},logger.v{time}(logger.c{time}{i},1),logger.v{time}(logger.c{time}{i},2),logger.v{time}(logger.c{time}{i},3),'Facecolor','g','Facealpha',alpha*0.3);
-        end
-        plot3(logger.cent{i,time}(1),logger.cent{i,time}(2),logger.cent{i,time}(3),'bo');
+        trisurf(logger.k{i,time},logger.v{time}(logger.c{time}{i},1),logger.v{time}(logger.c{time}{i},2),logger.v{time}(logger.c{time}{i},3),'Facecolor','r','Facealpha',alpha*0.3);
+%         plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'g.');
+            
+        
+        plot3(logger.cent{i,time}(1),logger.cent{i,time}(2),logger.cent{i,time}(3),'bo','MarkerSize',10);
     end
 
     hold off
+
+    if mod(time,(1/dt)*1) == 1
+        Fig.No = Fig. No + 1;
+        figure(Fig.No)
+        title(['t=',num2str(logger.time(time)),'s'],'FontSize',25);
+        hold on
+        
+        view(45,45);
+        daspect([1 1 1]);
+        ax = gca;
+        ax.Box = 'on';
+        ax.GridColor = 'k';
+        ax.GridAlpha = 0.4;
+        xlabel('x [m]','FontSize',pt);
+        ylabel('y [m]','FontSize',pt);
+        zlabel('z [m]','FontSize',pt);
+        xlim([-2,2]);
+        ylim([-2,2]);
+        zlim([-2,2]);
+        grid on
+
+        plot3(logger.P(time,1:3:end-2),logger.P(time,2:3:end-1),logger.P(time,3:3:end),'k*','MarkerSize',10); %エージェントの位置
+        for i = 1:N
+            trisurf(logger.k{i,time},logger.v{time}(logger.c{time}{i},1),logger.v{time}(logger.c{time}{i},2),logger.v{time}(logger.c{time}{i},3),'Facecolor','r','Facealpha',alpha*0.3);
+    %         plot3(logger.bx{i,time}(logger.zo{i,time},1),logger.bx{i,time}(logger.zo{i,time},2),logger.bx{i,time}(logger.zo{i,time},3),'g.');
+                
+            
+            plot3(logger.cent{i,time}(1),logger.cent{i,time}(2),logger.cent{i,time}(3),'bo','MarkerSize',10);
+        end
+
+        hold off
+    end
     time = time + 1;
 
     frame = getframe(figure(1));
@@ -131,3 +188,19 @@ while time <= numel(logger.time)
 end
 disp('simulation end');
 close(v);
+%% 自己位置と重心の推移
+figure(9) %agent1
+log_cent = cell2mat(logger.cent');
+hold on
+axis equal
+xlabel('t [s]','FontSize',pt);
+ylabel('Position [m]','FontSize',pt)
+xlim([0,logger.time(end)]);
+ylim([-2,2]);
+plot(logger.time,P(1:end-1,1));
+plot(logger.time,P(1:end-1,2));
+plot(logger.time,P(1:end-1,3));
+plot(logger.time,log_cent(:,1),'--');
+plot(logger.time,log_cent(:,2),'--');
+plot(logger.time,log_cent(:,3),'--');
+hold off
